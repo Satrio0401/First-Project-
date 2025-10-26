ba8ad88cc3acc707ad05db7fa708ef03cc145f09
Satrio0401 <satrioptk960@gmail.com>
2025-10-26 13:09:27 +0700

    add map section

---
+++ resources/views/livewire/map-page.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <!-- Header -->
+    <div class="bg-linear-to-r from-emerald-700 via-green-800 to-teal-900 text-white py-16">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 class="text-4xl font-bold mb-2">Galeri</h1>
+            <p class="text-blue-100">Rekam Jejak Perjuangan dalam Lensa.</p>
+        </div>
+    </div>
+
+    <!-- Konten -->
+
+</div>
